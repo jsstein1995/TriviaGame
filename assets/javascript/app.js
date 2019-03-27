60a3e9789f7b9d146336a76bfd44586f2f44bd45
Jackson Stein <jsstein1995@Jacksons-MBP.tx.rr.com>
2019-03-27 16:06:53 -0500

    12

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ window.onload = function () {
     $("#start").on("click", start);
 
 }
-var number = 10;
+var number = 120;
 var intervalId;
 
 function start() {
@@ -23,8 +23,6 @@ function decrement() {
     if (number === 0) {
         stop();
         endGame();
-        wrong++;
-        total++;
     }
 }
 
@@ -35,89 +33,106 @@ function stop() {
 
 function questions() {
 
-    // Change the HTML to show the question and answers.
-    $('#question1').html("1: Judith Keppel's questions: Complete the saying: 'As sick as a...'?");
-        $('<form ><input type="radio" name="radiobtn" value="Partridge">Partridge</input>').appendTo('#answer1');
-        $('<form ><input type="radio" name="radiobtn" value="Puffin">Puffin</input>').appendTo('#answer1');
-        $('<form ><input type="radio" name="radiobtn" value="Parrot">Parrot</input>').appendTo('#answer1');
-        $('<form ><input type="radio" name="radiobtn" value="Penguin">Penguin</input>').appendTo('#answer1');
-    
-    $('#question2').html("2: Which legal document states a person's wishes regarding the disposal of thier property after death?");
-        $('<form ><input type="radio" name="radiobtn" value="Would">Would</input>').appendTo('#answer2');
-        $('<form ><input type="radio" name="radiobtn" value="Shall">Shall</input>').appendTo('#answer2');
-        $('<form ><input type="radio" name="radiobtn" value="Should">Should</input>').appendTo('#answer2');
-        $('<form ><input type="radio" name="radiobtn" value="Will">Will</input>').appendTo('#answer2');
+    var trivia = [
+        {
+            q: "1: Judith Keppel's questions: Complete the saying: 'As sick as a...'?",
+            choices: ["Partridge", "Puffin", "Parrot", "Penguin"],
+            correctAnswer: "Parrot"
+        },
 
-    $('#question3').html("3: Complete the title of the James Bond film: 'The Man With the...'?");
-        $('<form ><input type="radio" name="radiobtn" value="Golden Tooth">Golden Tooth</input>').appendTo('#answer3');
-        $('<form ><input type="radio" name="radiobtn" value="Golden Gun">Golden Gun</input>').appendTo('#answer3');
-        $('<form ><input type="radio" name="radiobtn" value="Golden Eagle">Golden Eagle</input>').appendTo('#answer3');
-        $('<form ><input type="radio" name="radiobtn" value="Golden Delicious">Golden Delicious</input>').appendTo('#answer3');
+        {
+            q: "2: Which legal document states a person's wishes regarding the disposal of thier property after death?",
+            choices: ["Would", "Shall", "Should", "Will"],
+            correctAnswer: "Will"
+        },
 
-    $('#question4').html("4: Which of these fruits shares its name whith something superior or desirable?");
-        $('<form ><input type="radio" name="radiobtn" value="Apricot">Apricot</input>').appendTo('#answer4');
-        $('<form ><input type="radio" name="radiobtn" value="Grapefruit">Grapefruit</input>').appendTo('#answer4');
-        $('<form ><input type="radio" name="radiobtn" value="Plum">Plum</input>').appendTo('#answer4');
-        $('<form ><input type="radio" name="radiobtn" value="Mango">Mango</input>').appendTo('#answer4');
+        {
+            q: "3: Complete the title of the James Bond film: 'The Man With the...'?",
+            choices: ["Golden Tooth", "Golden Gun", "Golden Eagle", "Golden Delicious"],
+            correctAnswer: "Golden Gun"
+        },
 
-    $('#question5').html("5: In which sport do two teams pull at opposite ends of a rope?");
-        $('<form ><input type="radio" name="radiobtn" value="Tug of war">Tug of war</input>').appendTo('#answer5');
-        $('<form ><input type="radio" name="radiobtn" value="Basketball">Basketball</input>').appendTo('#answer5');
-        $('<form ><input type="radio" name="radiobtn" value="Ice hockey">Ice hockey</input>').appendTo('#answer5');
-        $('<form ><input type="radio" name="radiobtn" value="Polo">Polo</input>').appendTo('#answer5');
+        {
+            q: "4: Which of these fruits shares its name whith something superior or desirable?",
+            choices: ["Apricot", "Grapefruit", "Plum", "Mango"],
+            correctAnswer: "Plum"
+        },
 
-    $('#question6').html("6: Sherpas and Gurkas are native to which country?");
-        $('<form ><input type="radio" name="radiobtn" value="Russia">Russia</input>').appendTo('#answer6');
-        $('<form ><input type="radio" name="radiobtn" value="Ecuador">Ecuador</input>').appendTo('#answer6');
-        $('<form ><input type="radio" name="radiobtn" value="Nepal">Nepal</input>').appendTo('#answer6');
-        $('<form ><input type="radio" name="radiobtn" value="Morocco">Morocco</input>').appendTo('#answer6');
+        {
+            q: "5: In which sport do two teams pull at opposite ends of a rope?",
+            choices: ["Tug of war", "Basketball", "Ice hockey", "Polo"],
+            correctAnswer: "Tug of War"
+        },
 
-    $('#question7').html("7: Prime Minister Tony Blair was born in which country?");
-        $('<form ><input type="radio" name="radiobtn" value="England">England</input>').appendTo('#answer7');
-        $('<form ><input type="radio" name="radiobtn" value="Northern Ireland">Northern Ireland</input>').appendTo('#answer7');
-        $('<form ><input type="radio" name="radiobtn" value="Scotland">Scotland</input>').appendTo('#answer7');
-        $('<form ><input type="radio" name="radiobtn" value="Wales">Wales</input>').appendTo('#answer7');
+        {
+            q: "6: Sherpas and Gurkas are native to which country?",
+            choices: ["Russia", "Ecuador", "Nepal", "Morocco"],
+            correctAnswer: "Nepal"
+        },
 
-    $('#question8').html("8: Whose autobiography has the title 'A Long Walk To Freedom'?");
-        $('<form ><input type="radio" name="radiobtn" value="Ranulph Fennes">Ranulph Fennes</input>').appendTo('#answer8');
-        $('<form ><input type="radio" name="radiobtn" value="Mother Teresa">Mother Teresa</input>').appendTo('#answer8');
-        $('<form ><input type="radio" name="radiobtn" value="Nelson Mandela">Nelson Mandela</input>').appendTo('#answer8');
-        $('<form ><input type="radio" name="radiobtn" value="Mikhail Gorbachev">Mikhail Gorbachev</input>').appendTo('#answer8');
+        {
+            q: "7: Prime Minister Tony Blair was born in which country?",
+            choices: ["England", "Northern Ireland", "Scotland", "Wales"],
+            correctAnswer: "Scotland"
+        },
 
-    $('#question9').html("9: Duffel coats are named after a town in which country?");
-        $('<form ><input type="radio" name="radiobtn" value="Belgium">Belgium</input>').appendTo('#answer9');
-        $('<form ><input type="radio" name="radiobtn" value="Holland">Holland</input>').appendTo('#answer9');
-        $('<form ><input type="radio" name="radiobtn" value="Germany">Germany</input>').appendTo('#answer9');
-        $('<form ><input type="radio" name="radiobtn" value="Austria">Austria</input>').appendTo('#answer9');
+        {
+            q: "8: Whose autobiography has the title 'A Long Walk To Freedom'?",
+            choices: ["Ranulph Fennes", "Mother Teresa", "Nelson Mandela", "Mikhail Gorbachev"],
+            correctAnswer: "Nelson Mandela"
+        },
 
-    $('#question10').html("10: Complete this stage instruction in Shakespeare's 'The Winter's Tale': 'Exit, persued by a...'?");
-        $('<form ><input type="radio" name="radiobtn" value="Tiger">Tiger</input>').appendTo('#answer10');
-        $('<form ><input type="radio" name="radiobtn" value="Clown">Clown</input>').appendTo('#answer10');
-        $('<form ><input type="radio" name="radiobtn" value="Bear">Bear</input>').appendTo('#answer10');
-        $('<form ><input type="radio" name="radiobtn" value="Dog">Dog</input>').appendTo('#answer10');
+        {
+            q: "9: Duffel coats are named after a town in which country?",
+            choices: ["Belgium", "Holland", "Germany", "Austria"],
+            correctAnswer: "Belgium"
+        },
 
-        $('<button><input type="submit" name="submitbtn"></input>').appendTo(done);
+        {
+            q: "10: Complete this stage instruction in Shakespeare's 'The Winter's Tale': 'Exit, persued by a...'?",
+            choices: ["Tiger", "Clown", "Bear", "Dog"],
+            correctAnswer: "Would"
+        }
+    ]
+    var questionDiv = $("#questions")
+
+    for (var i = 0; i < trivia.length; i++) {
+        questionDiv.append("<h2>" + trivia[i].q + "</h2>");
+        for (var j = 0; j < trivia[i].choices.length; j++) {
+            questionDiv.append("<input type='radio' name='question-" + i +
+                "'value='" + trivia[i].choices[j] + "''>" + trivia[i].choices[j]);
+        }
     }
+    $("#done").append("<button id='done'>Done</button>");
+}
 
- // create done button and click event
- $("submitbtn").on('click', function(){
+// create done button and click event
+$("#done").on('click', function () {
     endGame();
-    
+});
 
- });
- 
-
- function endGame(){
+function endGame() {
+    clearInterval(intervalId);
+    var inputs = $("#questions").children("input:checked");
+    for (var i = 0; i < inputs.length; i++) {
+        if ($(inputs[i]).val() === trivia[i].correctAnswer) {
+            correct++;
+        } else {
+            wrong++;
+        }
+    }
     $("#questions").empty();
-    $("#questions").append("Correct: " + correct +" ");
-    $("#questions").append("Wrong: " + wrong+" ");
-    $("#questions").append("Total: " + total+" ");
- }
- 
+    $("#questions").append("Correct: " + correct + " ");
+    $("#questions").append("Wrong: " + wrong + " ");
+    $("#questions").append("Total: " + total + " ");
+
+
+}
 
 
 
- 
+
+
 
 
 
